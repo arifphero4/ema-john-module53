25b7e84bbc58e59b1a1b781c690e09c213241a55
arifphero <ahammadarif232@gmail.com>
2021-10-02 02:00:16 +0600

    work done

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const OrderReview = () => {
+    const [products] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        removeFromDb(key);
+
+
+    }
+
     return (
-        <div>
+        <div className="shop-container">
+            {/* <h1> Found items: {products.length}</h1>
+            <h3> cart items: {cart.length}</h3>
             <h2>This is Order Review</h2>
+            <Cart cart={cart}></Cart> */}
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key ={product.key}
+                        product={product} 
+                        handleRemove = {handleRemove}
+                    ></ReviewItem>)
+                }
+
+            </div>
+            <div className="cart-container">
+            <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
